f4b60945cb21a727d58ef64adfeb72b8b77be99d
Marcel Stringer <marcel.stringer@gmail.com>
2015-01-31 22:13:00 +0100

    Changed world to Marcel

---
+++ eclipse/testproject/src/testproject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : testproject.cpp
+// Author      : Marcel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello Marcel" << endl; // prints Hello World
+	return 0;
+}
