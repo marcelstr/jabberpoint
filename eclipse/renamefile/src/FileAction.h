89bd8fed9d9e6f4f2739338c959ffc03b3315445
MarcelS <MarcelS@192.168.1.37>
2018-03-12 20:54:07 +0100

    Initial checkin

---
+++ eclipse/renamefile/src/FileAction.h
@@ -0,0 +1,20 @@
+/*
+ * FileAction.h
+ *
+ *  Created on: 19 feb. 2016
+ *      Author: MarcelS
+ */
+
+#ifndef FILEACTION_H_
+#define FILEACTION_H_
+
+
+class FileAction
+{
+    public:
+
+    virtual void Execute() = 0;
+};
+
+
+#endif /* FILEACTION_H_ */
